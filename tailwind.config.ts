5fb0bb8bb542693cb1fd4445324a451cd7026835
Arie Setiadi <arie@eannovate.com>
2024-06-21 09:05:36 +0800

    setup tailwind color palette

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,24 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+    content: [
+        './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+        './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+        './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    ],
+    theme: {
+        colors: {
+            'color-primary': '#912728',
+            'color-primary-50': '#9127297a',
+            'color-primary-dark': '#441E15',
+            'color-secondary': '#FAA370',
+            'color-secondary-light': '#FEEDE2',
+            'color-danger': '#C84108',
+            'color-light': '#FAF2F1',
+            'color-white': '#FFFFFF',
+            'color-dark': '#333333',
+        },
     },
-  },
-  plugins: [],
+    plugins: [],
 };
 export default config;
